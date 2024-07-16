554d7f7c5811aceb1ba5556e296dd38092c3cd36
robin.li <robin.li@webeye.com>
2024-07-16 09:58:31 +0800

    feat: add sitemap

---
+++ app/sitemap..ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next'
+const HOST = 'https://chromeai.tech'
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: HOST,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url:`${HOST}/chat`,
+            priority: 1,
+        },
+        {
+            url:`${HOST}/rag`,
+            priority: 1,
+        },
+        {
+            url:`${HOST}/setup`,
+            priority: 1,
+        },
+        {
+            url:`${HOST}/policy`,
+            priority: 0.1,
+        },
+        {
+            url:`${HOST}/tos`,
+            priority: 0.1,
+        },
+    ]
+}
